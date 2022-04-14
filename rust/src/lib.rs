17058d55c45f09b2a9cf2216a9a4b76769af7317
unknown <conghuy649@gmail.com>
2022-04-14 16:47:27 +0700

    add something

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+// #[macro_use]
+// extern crate gdnative;
+
+use gdnative::{prelude::*, api::TileMap};
+
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct HelloWorld;
+
+#[methods]
+impl HelloWorld {
+    fn new(_owner: &Node) -> Self{
+        HelloWorld
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node){
+        godot_print!("Something");
     }
 }
+
+#[derive(NativeClass)]
+#[inherit(gdnative::api::TileMap)]
+pub struct Map;
+
+#[methods]
+impl Map{
+    fn new(_owner: &TileMap)->Self{
+        Map
+    }
+}
+
+
+fn init(handle: InitHandle){
+    handle.add_class::<HelloWorld>();
+    handle.add_class::<Map>();
+}
+
+
+
+
+godot_init!(init);
